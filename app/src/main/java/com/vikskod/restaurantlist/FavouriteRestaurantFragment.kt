cc399df9b897e9b759e1694a24938f98a38a0f61
vikash <vparajuli819@gmail.com>
2021-02-10 14:34:55 +1100

    Dependencies added, bottom navigation added with fragments

---
+++ app/src/main/java/com/vikskod/restaurantlist/FavouriteRestaurantFragment.kt
@@ -0,0 +1,17 @@
+package com.vikskod.restaurantlist
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class FavouriteRestaurantFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_favourite_restaurant, container, false)
+    }
+}
--- app/src/main/java/com/vikskod/restaurantlist/MainActivity.kt
+++ app/src/main/java/com/vikskod/restaurantlist/MainActivity.kt
@@ -1,11 +1,69 @@
 package com.vikskod.restaurantlist
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.postDelayed
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.vikskod.restaurantlist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupViews()
+    }
+
+    fun setupViews() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragNavHost) as NavHostFragment
+        navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(binding.bottomNavView, navHostFragment.navController)
+        val appBarConfiguration =
+            AppBarConfiguration(setOf(R.id.allRestaurantFragment, R.id.favouriteRestaurantFragment))
+        setupActionBarWithNavController(navHostFragment.navController, appBarConfiguration)
+    }
+
+    fun showBottomNavigation() {
+        binding.bottomNavView.visibility = View.VISIBLE
+    }
+
+    fun hideBottomNavigation() {
+        binding.bottomNavView.visibility = View.GONE
+    }
+
+    private var backPressedOnce = false
+
+    override fun onBackPressed() {
+        if (navController.graph.startDestination == navController.currentDestination?.id) {
+            if (backPressedOnce) {
+                super.onBackPressed()
+                return
+            }
+
+            backPressedOnce = true
+            Toast.makeText(this, "Press BACK again to exit", Toast.LENGTH_SHORT).show()
+
+            Handler(Looper.getMainLooper()).postDelayed(2000) {
+                backPressedOnce = false
+            }
+        } else super.onBackPressed()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
     }
 }
